75a9e8fbef9b2cbe1490a12e428b9aed81f6abb4
Kluckmuck <kluckmucki@gmail.com>
2018-06-15 14:37:11 +0200

    stores field names in fields variable. Use that to check if its null or blank. Then it should only draw qualified fields

--- backend/booking/views_pdf.py
+++ backend/booking/views_pdf.py
@@ -9,18 +9,26 @@ def createPdf(request, bokningId):
         id = int(bokningId)
         try:
             bokning = Bokning.objects.get(id=id)
+            fields = [f.name for f in bokning._meta.get_fields()]
+            for n in fields:
+                #if bokning.name != None
+                print(bokning.n)
+
         except Bokning.DoesNotExist:
             return HttpResponseNotFound()
         # Create the HttpResponse object with the appropriate PDF headers.
+        # Användar ser pdf i webbläsaren.
         response = HttpResponse(content_type='application/pdf')
-        response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
+        #response['Content-Disposition'] = 'attachment; filename="somefilename.pdf"'
 
+        # Skapar pdf lokalt. Används för tester.
         # Create the PDF object, using the response object as its "file."
-        p = canvas.Canvas(response)
+        p = canvas.Canvas("bookning.pdf")
 
         # Draw things on the PDF. Here's where the PDF generation happens.
         # See the ReportLab documentation for the full list of functionality.
         p.drawString(100, 100, "Hello world.")
+
 
         # Close the PDF object cleanly, and we're done.
         p.showPage()
