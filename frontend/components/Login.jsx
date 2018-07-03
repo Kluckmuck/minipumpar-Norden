03f8b6cae949bbb59f386e9c3ab57f790acf9e63
maxjou <max.jourdanis@gmail.com>
2018-07-03 18:32:38 +0200

    add dockerfile frontend

--- frontend/components/Login.jsx
+++ frontend/components/Login.jsx
@@ -2,6 +2,24 @@ import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "./css/Login.css";
 import { withRouter } from "react-router-dom";
+import jQuery from 'jquery';
+
+var site  = 'http://maxjou.se:8000';
+var csrftoken = getCookie('csrftoken');
+function getCookie(name) {
+  var cookieValue = null;
+  if (document.cookie && document.cookie !== '') {
+      var cookies = document.cookie.split(';');
+      for (var i = 0; i < cookies.length; i++) {
+          var cookie = jQuery.trim(cookies[i]);
+          if (cookie.substring(0, name.length + 1) === (name + '=')) {
+              cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+              break;
+          }
+      }
+  }
+  return cookieValue;
+}
 
  class Login extends Component{
   constructor(props) {
@@ -23,8 +41,32 @@ import { withRouter } from "react-router-dom";
  }
 
   handleSubmit(event) {
+
+
+    fetch (site + '/api/login/', {
+      credentials: 'include',
+      method: 'post',
+      headers: {
+        'Accept': 'text/plain',
+        'Content-Type': 'application/json',
+        'X-CSRFToken': csrftoken
+      },
+      body: JSON.stringify({
+        username: this.state.email,
+        password: this.state.password
+      })
+    }).then(response => {
+      response.json().then(data => ({
+        data: data,
+        status: response.status
+      })
+    ).then(res =>{
+      console.log(res.status, res.data.title)
+    })
+  })
+
     event.preventDefault();
-    this.props.history.push('/inputs')
+    // this.props.history.push('/inputs')
     console.log('Hello')
   }
   render() {
