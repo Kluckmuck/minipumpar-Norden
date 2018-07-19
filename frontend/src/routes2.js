016c44127ba34e89fd9c9409bc9d3e551a3ee16d
Max Jourdanis <max.jourdanis@gmail.com>
2018-07-19 17:37:38 +0200

    Add separate routes.

--- frontend/src/routes2.js
+++ frontend/src/routes2.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// import ReactDOM from 'react-dom';
 import Login from "./components/Login.jsx";
 import Inputs from "./components/Inputs.jsx";
 import { HashRouter as Router, Route } from 'react-router-dom';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import Settings from "./components/Settings.jsx";
 // import routes from "./routes";
-ReactDOM.render(  <Router>
+export default  ( <Router>
     <div>
       <Route exact path="/" component={Login}></Route>
       <Route path="/inputs" component={Inputs}></Route>
       <Route path="/settings" component={Settings}></Route>
     </div>
-</Router>, document.getElementById('app'));
-registerServiceWorker();
+</Router>)
