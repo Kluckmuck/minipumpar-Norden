df572eaf2fafffa8712acd1c7b7ab20224a2e7d3
Max Jourdanis <maxjou@localhost.localdomain>
2018-07-19 10:26:44 +0200

    Fix -nav and login

--- frontend/src/NavBar.jsx
+++ frontend/src/NavBar.jsx
@@ -1,21 +1,31 @@
 
 import React, {Component }from "react";
 import { Link } from "react-router-dom";
-import {  Navbar } from "react-bootstrap";
+import {  Navbar , NavItem, Nav, NavDropdown, MenuItemLink, MenuItem} from "react-bootstrap";
 import "./NavBar.css";
+var FontAwesome = require('react-fontawesome');
  class NavBar extends Component {
-    render (){
-        return  <div className="container">
+   render (){
+   return  <div className="container">
        <Navbar fluid collapseOnSelect>
          <Navbar.Header>
            <Navbar.Brand>
-             <Link to="/">NavBar</Link>
+             <Link to="/inputs"><FontAwesome icon="spinner" listItem />Minipumpar</Link>
            </Navbar.Brand>
            <Navbar.Toggle />
          </Navbar.Header>
+         <Navbar.Collapse>
+           <Nav>
+           <NavDropdown title="Inställningar" >
+           
+             <Link to="/"> <MenuItem>Settings</MenuItem></Link>
+           </NavDropdown>
+          </Nav>
+         </Navbar.Collapse>
        </Navbar>
      </div>
     }
 }
 
 export default NavBar;
+
