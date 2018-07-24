5d0f34066dfdb678f7bbdf3e2550d7d1a9c21963
Kluckmuck <kluckmucki@gmail.com>
2018-07-24 15:00:41 +0200

    trying with django-react-csrf cokie and fetching with the js-cookie module

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,18 +1,12 @@
 import React, { Component } from "react";
 import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./Login.css";
 import { withRouter } from "react-router-dom";
-import DjangoCSRFToken from 'django-react-csrftoken';
 import Cookies from 'js-cookie';
+import DjangoCSRFToken from 'django-react-csrftoken'
+import "./Login.css";
 
 var site  = 'http://maxjou.se:8000';
-
-let header = new Headers({
-  "Content-Type": "application/json; charset=utf-8",
-  "Access-Control-Request-Headers": "*",
-  "Access-Control-Allow-Methods": "GET, POST, HEAD, OPTIONS, PUT, DELETE, PATCH",
-  "X-CSRFToken": Cookies.get('csrftoken')
-});
+var csrftoken = Cookies.get('csrftoken');
 
  class Login extends Component{
   constructor(props) {
@@ -41,7 +35,11 @@ let header = new Headers({
     fetch (site + '/api/login/', {
 
       method: 'post',
-      header: header,
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json; charset=utf-8',
+        'X-CSRFToken': csrftoken
+      },
       body: JSON.stringify({
         username: this.state.username,
         password: this.state.password
@@ -54,7 +52,7 @@ let header = new Headers({
       }else if (response.status === 204){
         console.log("Username passwod do not match");
       }else {
-        console.log("username does not exist");
+        console.log(csrftoken);
       }
     })
   }
@@ -63,32 +61,32 @@ let header = new Headers({
   <div className="Login">
     <h1>Login</h1>
       <form onSubmit={this.handleSubmit}>
-          <DjangoCSRFToken/>
-          <FormGroup controlId="username" bsSize="large">
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              value={this.state.username}
-              onChange={this.handleChange}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-            />
-          </FormGroup>
-          <Button
-            block
-            bsSize="large"
-            disabled={!this.validateForm()}
-            type="submit"
-          >
-            Login
-          </Button>
+        <DjangoCSRFToken/>
+        <FormGroup controlId="username" bsSize="large">
+          <ControlLabel>Email</ControlLabel>
+          <FormControl
+            autoFocus
+            type="email"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+        </FormGroup>
+        <FormGroup controlId="password" bsSize="large">
+          <ControlLabel>Password</ControlLabel>
+          <FormControl
+            value={this.state.password}
+            onChange={this.handleChange}
+            type="password"
+          />
+        </FormGroup>
+        <Button
+          block
+          bsSize="large"
+          disabled={!this.validateForm()}
+          type="submit"
+        >
+          Login
+        </Button>
       </form>
   </div>
   );
