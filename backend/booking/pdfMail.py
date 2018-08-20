53c7eef2d27d98a6fbf809b47f09961eb714ad22
Kluckmuck <kluckmucki@gmail.com>
2018-08-20 14:29:57 +0200

    Each field now has a proper swedish name. Field value is processed, adding stuff like meters, timmar and m3. Fixed some bugs that aligned lines poorly. Header is using a larger font now. Footer has a moose pic and text below

--- backend/booking/pdfMail.py
+++ backend/booking/pdfMail.py
@@ -67,7 +67,7 @@ def createPdf(bokning, response=None):
         filePath = "pdf/" + str(bokning.datum) + "(" + str(bokning.id) + ").pdf"
         c = canvas.Canvas(filePath)
     #Font
-    c.setFont(pdf.font, 12)
+    c.setFont(pdf.font, pdf.size)
     drawHeader(c)
     drawFields(c, bokning)
     drawFooter(c)
@@ -80,13 +80,17 @@ def createPdf(bokning, response=None):
 
 def drawHeader(c):
     #Header for PDF
-    c.drawString(pdf.x, pdf.y, "Telefon: 070-557 66 38 - info@minipumpar - orgnr 556851-2809")
-    pdf.y = pdf.y - 40
+    c.setFont(pdf.font, pdf.size + 3)
+    c.drawString(pdf.x, pdf.y, "Telefon: 070-557 66 38 - info@minipumpar.se - orgnr 556851-2809")
+    pdf.y = pdf.y - 60
 
 def drawFooter(c):
     #Footer for PDF
     c.setFont("Helvetica-Oblique", pdf.size)
-    c.drawString(pdf.x, 20, "Utvecklad av Älg IT Handelsbolag, 0706566805")
+    c.drawString(pdf.x+380, 25, "Utvecklad av Älg IT")
+    c.drawString(pdf.x+380, 10, "070-656 68 05")
+    logo = os.path.abspath('./static/images/Watercolor_Moose.jpg')
+    c.drawImage(logo,x+330,50, 150, 150)
 
 def getHourMinute(time):
     #Appends zero if minute is 0-9
@@ -96,9 +100,13 @@ def getHourMinute(time):
     time = str(time.hour) + ':' + minute
     return time
 
-def drawValue(c,string):
-    c.setFont(pdf.font, pdf.size)
-    c.drawString(pdf.x+pdf.lineWidth, pdf.y,string)
+def drawValue(c,string, bold=False):
+    if bold == True:
+        c.setFont(pdf.fontBold, pdf.size)
+        c.drawString(pdf.x, pdf.y,string)
+    else:
+        c.setFont(pdf.font, pdf.size)
+        c.drawString(pdf.x+pdf.lineWidth, pdf.y,string)
 
 def drawFields(c, bokning):
     #Body of PDF
@@ -106,30 +114,51 @@ def drawFields(c, bokning):
     fields = [f.name for f in bokning._meta.get_fields()]
     for i,n in enumerate(fields):
         #Draws each field name in bold text & capitalizes the first letter
-        c.setFont(fontBold, size)
-        c.drawString(pdf.x,pdf.y,fields[i].title() + ':')
-        if fields[i] is 'pumpStart':
+        if fields[i] is 'id':
+            drawValue(c, 'Följesedels nr:', True)
+        elif fields[i] is 'slangStr':
+            drawValue(c, 'Slang längd:', True)
+        elif fields[i] is 'pumpMng':
+            drawValue(c, 'Pump mängd:', True)
+        elif fields[i] is 'littNr':
+            drawValue(c, 'Litt nr:', True)
+        elif fields[i] is 'pumpStart':
+            drawValue(c, 'Pump start:', True)
+            #Special case because of date
             drawValue(c,getHourMinute(getattr(bokning,fields[i])))
         elif fields[i] is 'pumpSlut':
+            drawValue(c, 'Pump slut:', True)
+            #Special case because of date
             drawValue(c,getHourMinute(getattr(bokning,fields[i])))
         #Draw total time draw
         elif fields[i] is 'ovrigInfo':
-            drawValue(c,str(getattr(bokning,fields[i])))
-            pdf.y = pdf.y - pdf.lineHeight
             c.setFont(fontBold, size)
             c.drawString(pdf.x,pdf.y, 'Total tid')
             c.setFont(font, size)
             startTime = getattr(bokning,fields[i-2])
             endTime = getattr(bokning,fields[i-1])
             drawValue(c,str(endTime-startTime)[:-3])
-        #Draws each field value to the right of field name
+            pdf.y = pdf.y - pdf.lineHeight
+            drawValue(c, 'Övrig info:', True)
+        #Draws each field value that is not caught before
         else:
-            value = str(getattr(bokning,fields[i]))
-            if (value == "None"):
-                #If the field value is None, dont draw
-                pass
-            else:
-                drawValue(c,value)
+            c.setFont(fontBold, size)
+            c.drawString(pdf.x,pdf.y,fields[i].title() + ':')
+        value = getattr(bokning,fields[i])
+        if fields[i] is 'grundavgift':
+            value = str(value) + ' kr'
+        if fields[i] is 'resTid':
+            value = str(value) + ' timmar'
+        if fields[i] is 'pumpMng':
+            int(value)
+            value = str(value) + ' m3'
+        if fields[i] is 'slangStr':
+            value = str(value) + ' meter'
+        if (value == None or isinstance(value, datetime)):
+            #If the field value is None or date(date is drawed before), dont draw
+            pass
+        else:
+            drawValue(c,str(value))
         pdf.y = pdf.y - pdf.lineHeight
 
 def resetPdf():
